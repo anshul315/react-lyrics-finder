5c0b7058f20e407b210eb7cd610f71d7a74000f4
Anshul <admin@syncspire.com>
2019-06-16 16:36:37 +0530

    Add Tracks component and basic routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
-import Navbar from './components/layout/Navbar'
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-    </div>
-  );
+import Navbar from './components/layout/Navbar';
+import Index from './components/layout/Index';
+
+class App extends Component {
+  render(){
+    return (
+      <Router>
+        <React.Fragment>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Index} />
+            </Switch>
+          </div>
+        </React.Fragment>
+      </Router>
+    );
+  }
 }
 
 export default App;
